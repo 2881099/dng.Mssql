a35635929e0f0d1060f1290670cc2d3127551483
28810 <28810@YEXIANGQIN>
2019-03-22 16:42:44 +0800

    upgrade nuget packages

--- Mssql/SqlConnectionPool.cs
+++ Mssql/SqlConnectionPool.cs
@@ -42,6 +42,7 @@ namespace System.Data.SqlClient {
 		public string Name { get; set; } = "SQLServer SqlConnection 对象池";
 		public int PoolSize { get; set; } = 100;
 		public TimeSpan SyncGetTimeout { get; set; } = TimeSpan.FromSeconds(10);
+		public TimeSpan IdleTimeout { get; set; } = TimeSpan.Zero;
 		public int AsyncGetCapacity { get; set; } = 10000;
 		public bool IsThrowGetTimeoutException { get; set; } = true;
 		public int CheckAvailableInterval { get; set; } = 5;
